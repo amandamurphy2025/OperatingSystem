9adf55de4df16d2a5e0b2014284e44dcfd93a506
Amanda Murphy <akmurphak@linux7.cs.uchicago.edu>
2024-10-28 12:23:20 -0500

    prepping for small pull disregard me

--- pintos-p2/pintos-raw/src/threads/thread.c
+++ pintos-p2/pintos-raw/src/threads/thread.c
@@ -464,6 +464,12 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->magic = THREAD_MAGIC;
 
+  //AMANDA ADDED HERE
+  t->next_file = 2;
+  for (int i=0; i < MAX_FILES; i++){
+    t->files[i] = NULL;
+  }
+
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
